2c40c85cb69fb48a73f1d29f7cc3c351685a41b4
MikelDev <millymike599@gmail.com>
2024-08-14 03:41:34 +0100

    store components in its own folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreConcert({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </li>
-  );
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 
 function App() {
   return (
@@ -40,10 +10,10 @@ function App() {
         <section id="core-concepts">
           <h2> Core Concept</h2>
           <ul>
-            <CoreConcert {...CORE_CONCEPTS[0]} />
-            <CoreConcert {...CORE_CONCEPTS[1]} />
-            <CoreConcert {...CORE_CONCEPTS[2]} />
-            <CoreConcert {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <h2>Time to get started!</h2>

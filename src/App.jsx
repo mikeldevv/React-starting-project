80f3a05bca7ff6872bfd531fb0ab9fe9cd5af3b4
MikelDev <millymike599@gmail.com>
2024-08-14 02:44:58 +0100

    working with props in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcert({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </li>
+  );
 }
 
 function Header() {
@@ -26,6 +37,15 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2> Core Concept</h2>
+          <ul>
+            <CoreConcert {...CORE_CONCEPTS[0]} />
+            <CoreConcert {...CORE_CONCEPTS[1]} />
+            <CoreConcert {...CORE_CONCEPTS[2]} />
+            <CoreConcert {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
